fb300dac73b87abf3eee8c92c21f0d30ef239ca1
虫賀誠悟 <mushigaseigo@mushigamakotosatorunoMacBook-Pro.local>
2021-10-18 13:41:21 +0900

    Chapter04

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name1 = "虫賀誠悟";
+		System.out.println(name1);
+		
+		name1 = "一歩前進";
+		System.out.println(name1);
+		
+		final int lucky_number;
+		lucky_number = 7;
+		System.out.println(lucky_number);
+	}
+}
