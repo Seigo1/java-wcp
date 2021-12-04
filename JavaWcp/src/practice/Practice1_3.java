3dff396b4208be553868b1e9c41f4b4be5f22e0c
Seigo1 <champion.1995.08.28@gmail.com>
2021-12-04 21:50:54 +0900

    Practice

--- JavaWcp/src/practice/Practice1_3.java
+++ JavaWcp/src/practice/Practice1_3.java
@@ -1,5 +1,10 @@
 package practice;
 
 public class Practice1_3 {
-
+	public static void main(String[] args) {
+		int x = 13;
+		int y = 17;
+		System.out.println("x=" + x);
+		System.out.println("x=" + y);
+	}
 }
---
+++ JavaWcp/src/practice/Practice1_4.java
@@ -0,0 +1,8 @@
+package practice;
+
+public class Practice1_4 {
+	public static void main(String[] args) {
+		int x = 13 + 17;
+		System.out.println(x);
+	}
+}
